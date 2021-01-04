f6c788c6250abed7e26e541811684fc9dd8ab3ae
MBR-0001 <55142207+MBR-0001@users.noreply.github.com>
2021-01-04 19:30:19 +0100

    feat(MessageCreate): support for DM messages

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -1,38 +1,39 @@
 'use strict';
 
 const Action = require('./Action');
-const { Events } = require('../../util/Constants');
+const { Events, ChannelTypes } = require('../../util/Constants');
 
 class MessageCreateAction extends Action {
   handle(data) {
     const client = this.client;
-    const channel = client.channels.cache.get(data.channel_id);
-    if (channel) {
-      const existing = channel.messages.cache.get(data.id);
-      if (existing) return { message: existing };
-      const message = channel.messages.add(data);
-      const user = message.author;
-      let member = message.member;
-      channel.lastMessageID = data.id;
-      if (user) {
-        user.lastMessageID = data.id;
-        user.lastMessageChannelID = channel.id;
-      }
-      if (member) {
-        member.lastMessageID = data.id;
-        member.lastMessageChannelID = channel.id;
-      }
+    let channel = client.channels.cache.get(data.channel_id);
 
-      /**
-       * Emitted whenever a message is created.
-       * @event Client#message
-       * @param {Message} message The created message
-       */
-      client.emit(Events.MESSAGE_CREATE, message);
-      return { message };
+    if (!channel && !data.guild_id) {
+      channel = client.channels.add({ id: data.channel_id, type: ChannelTypes.DM, recipients: [data.author] });
     }
 
-    return {};
+    const existing = channel.messages.cache.get(data.id);
+    if (existing) return { message: existing };
+    const message = channel.messages.add(data);
+    const user = message.author;
+    let member = message.member;
+    channel.lastMessageID = data.id;
+    if (user) {
+      user.lastMessageID = data.id;
+      user.lastMessageChannelID = channel.id;
+    }
+    if (member) {
+      member.lastMessageID = data.id;
+      member.lastMessageChannelID = channel.id;
+    }
+
+    /**
+     * Emitted whenever a message is created.
+     * @event Client#message
+     * @param {Message} message The created message
+     */
+    client.emit(Events.MESSAGE_CREATE, message);
+    return { message };
   }
 }
 
